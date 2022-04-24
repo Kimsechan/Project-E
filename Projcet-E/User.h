8ceb53db8ffc53491492687a0ff1e1aa14c1998c
Kimsechan <ksc03030919@gmail.com>
2022-04-24 10:05:58 +0900

    User_Header

---
+++ Projcet-E/User.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class User
+{
+	int fdNumber;
+	wstring name = "<NULL>";
+
+public:
+	User(int targetFD)
+	{
+		fdNumber = targetFD;
+	}
+
+	void SetName(wstring wantName) { name = wantName; };
+};
