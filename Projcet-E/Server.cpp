d9a32657ca2dcea0d4769755c47496b9a4ba752f
Kimsechan <ksc03030919@gmail.com>
2022-05-28 11:44:14 +0900

    Update Server.cpp

--- Projcet-E/Server.cpp
+++ Projcet-E/Server.cpp
@@ -251,6 +251,7 @@ int main()
 		//엇.. 누가 있어? 닫아!
 		if (pollFDArray[i].fd != -1) close(pollFDArray[i].fd);
 	};
+
 	//두 개의 쓰레드를 전부 꺼줍니다
 	void* threadResult;
 	pthread_join(sendThread, &threadResult);
